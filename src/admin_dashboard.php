484860b8750bb26e2307a90cd5ca56680244fe6c
leolelion <leokaiya@proton.me>
2025-04-10 19:27:18 -0700

    Path update

--- src/admin_dashboard.php
+++ src/admin_dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../config.php';
+require_once 'config.php';
 
 // Check if user is logged in and is an admin
 // if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
