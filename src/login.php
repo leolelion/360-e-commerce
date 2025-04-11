de6c1c90c384755d18ec221e610ab97247a2469a
leolelion <leokaiya@proton.me>
2025-04-11 00:57:39 -0700

    Added header to login page

--- src/login.php
+++ src/login.php
@@ -6,11 +6,12 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login - ShopCo</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
+    <?php include 'header.php'; ?>
 
     <main>
         <div class="registration-form">
